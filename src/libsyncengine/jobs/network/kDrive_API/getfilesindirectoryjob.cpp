9f747fa3b66b8480d02ad1da170499cbb2ee7bf5
Luc Guyot <luc.guyot@infomaniak.com>
2026-02-10 10:12:56 +0100

    feat(get-file-in-root-dir): implements a job that retrieves the list of the files located at the root of the remote drive together with the files located inside the user private root folder

--- src/libsyncengine/jobs/network/kDrive_API/getfilesindirectoryjob.cpp
+++ src/libsyncengine/jobs/network/kDrive_API/getfilesindirectoryjob.cpp
@@ -27,25 +27,25 @@
 namespace KDC {
 
 GetFilesInDirectoryJob::GetFilesInDirectoryJob(const int userDbId, const int driveId, NodeId fileId, std::string cursorInput,
-                                               const bool dirOnly /*= false*/) :
+                                               const bool dirOnly /*= false*/, const bool translateV2ToV3 /*= false */) :
     AbstractTokenNetworkJob(ApiType::Drive, userDbId, 0, 0, driveId),
     _fileId(std::move(fileId)),
     _cursorInput(std::move(cursorInput)),
     _dirOnly(dirOnly) {
     _apiVersion = 3;
     _httpMethod = Poco::Net::HTTPRequest::HTTP_GET;
-    ApiTranslator::translateV2ToV3(driveDbId(), _fileId);
+    if (translateV2ToV3) ApiTranslator::translateV2ToV3(driveDbId(), _fileId);
 }
 
 GetFilesInDirectoryJob::GetFilesInDirectoryJob(const int driveDbId, NodeId fileId, std::string cursorInput,
-                                               const bool dirOnly /*= false*/) :
+                                               const bool dirOnly /*= false*/, const bool translateV2ToV3 /*= false */) :
     AbstractTokenNetworkJob(ApiType::Drive, 0, 0, driveDbId, 0),
     _fileId(std::move(fileId)),
     _cursorInput(std::move(cursorInput)),
     _dirOnly(dirOnly) {
     _apiVersion = 3;
     _httpMethod = Poco::Net::HTTPRequest::HTTP_GET;
-    ApiTranslator::translateV2ToV3(driveDbId, _fileId);
+    if (translateV2ToV3) ApiTranslator::translateV2ToV3(driveDbId, _fileId);
 }
 
 std::string GetFilesInDirectoryJob::getSpecificUrl() {
